0ae2d74b5a0dca4e234c6a988bf506196db70d3d
Cíntia Pinho <cintia.pinho01@gmail.com>
2019-07-16 14:27:17 -0300

    mockando lista de tarefas para pre-visualizar interface

--- src/app/tasks/pages/tasks-list/tasks-list.page.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.page.html',
-  styleUrls: ['./tasks-list.page.scss'],
+  styleUrls: ['./tasks-list.page.scss']
 })
 export class TasksListPage implements OnInit {
+  tasks$: Observable<Task[]>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.tasks$ = of([
+      { id: 'fd8g7df8g', title: 'Aprender Ionic', done: false },
+      { id: 'fd8g778df', title: 'Aprender Firestore', done: false }
+    ]);
   }
-
 }
