8256b032665e304b1326675009c4737d0c6dda03
Cíntia Pinho <cintia.pinho01@gmail.com>
2019-07-16 20:09:48 -0300

    Firestore: criando novos registros

--- src/app/core/classes/firestore.ts
+++ src/app/core/classes/firestore.ts
@@ -1,7 +1,7 @@
 import { AngularFirestore, AngularFirestoreCollection, QueryFn } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-export abstract class Firestore<T> {
+export abstract class Firestore<T extends { id: string }> {
   protected collection: AngularFirestoreCollection<T>;
 
   constructor(protected db: AngularFirestore) {}
@@ -9,8 +9,18 @@ export abstract class Firestore<T> {
   protected setCollection(path: string, queryFn?: QueryFn): void {
     this.collection = path ? this.db.collection(path, queryFn) : null;
   }
+  private setItem(item: T, operation: 'set' | 'update'): Promise<T> {
+    return this.collection
+      .doc<T>(item.id)
+      [operation](item)
+      .then(() => item);
+  }
 
   getAll(): Observable<T[]> {
     return this.collection.valueChanges();
+  }
+  create(item: T): Promise<T> {
+    item.id = this.db.createId();
+    return this.setItem(item, 'set');
   }
 }
